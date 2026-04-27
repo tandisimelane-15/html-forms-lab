40bb26c2648f5bfde35444e8156fc241fb7bd972
Abigail Tandiwe Simelane <tandisimelane24@gmail.com>
2026-04-27 21:31:44 +0300

    Final changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.getElementById('create-task-form')
+
+  form.addEventListener('submit' ,(event) => {
+    event.preventDefault()
+    const taskDescription = document.getElementById('new-task-description').value
+    buildToDo(taskDescription)
+  })
+
+})
+
+function buildToDo(task) {
+  const li = document.createElement('li')
+  li.textContent = task
+  document.getElementById('tasks').appendChild(li)
+}
